2bd94992295f5f18b7e430f867aa1b2231cc21e3
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2022-04-08 15:41:45 -0300

    feat: :sparkles: Utilizando Fontes Personalizadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import { ThemeProvider } from "styled-components/native";
 import theme from "./global/styles/theme";
+import AppLoading from "expo-app-loading";
+import { useFonts, Poppins_400Regular, Poppins_500Medium, Poppins_700Bold } from "@expo-google-fonts/poppins";
 import { Dashboard } from "./screens/Dashboard";
 
 export default function App() {
+    const [fontsLoaded] = useFonts({ Poppins_400Regular, Poppins_500Medium, Poppins_700Bold });
+
+    if (!fontsLoaded) {
+        return <AppLoading></AppLoading>;
+    }
+    
     return (
         <ThemeProvider theme={theme}>
-            <Dashboard></Dashboard>
+            <Dashboard />
         </ThemeProvider>
     );
 }
