96fa0ba4292c21eab6e59d179f0dd854063350e3
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2022-04-08 18:06:24 -0300

    feat: :sparkles: Tipando e acessando propriedades no Styled Components

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -28,10 +28,20 @@ export function Dashboard() {
                     <Icon name="power"></Icon>
                 </UserWrapper>
             </Header>
-            <HighlightCards >
-                <HighlightCard></HighlightCard>
-                <HighlightCard></HighlightCard>
-                <HighlightCard></HighlightCard>
+            <HighlightCards>
+                <HighlightCard
+                    type="up"
+                    title="Entradas"
+                    amount="R$ 17.400,00"
+                    lastTransaction="Última entrada dia 13 de abril"
+                />
+                <HighlightCard
+                    type="down"
+                    title="Saídas"
+                    amount="R$ 1.259,00"
+                    lastTransaction="Última saída dia 03 de abril"
+                />
+                <HighlightCard type="total" title="Total" amount="R$ 16.141,00" lastTransaction="1 á 6 de abril" />
             </HighlightCards>
         </Container>
     );
